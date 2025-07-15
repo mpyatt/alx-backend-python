c65ed266c8afcafd8455a0d21304a23738b70be3
Mike Attara <mikeattara.gh@gmail.com>
2025-07-15 09:46:48 +0000

    fix decorator

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,8 +6,6 @@ from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Tests for access_nested_map"""
-
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
