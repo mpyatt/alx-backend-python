0b150118b9982d9b18a292d360ef3f6980afbb70
Mike Attara <mikeattara.gh@gmail.com>
2025-07-15 10:18:54 +0000

    mocking

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -42,3 +42,23 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_org.return_value = {"repos_url": url}
             client = GithubOrgClient("testorg")
             self.assertEqual(client._public_repos_url, url)
+
+    @patch("client.get_json")
+    def test_public_repos(self, mock_get_json):
+        """
+        Test that public_repos returns the correct list of repo names.
+        """
+        mock_get_json.return_value = [
+            {"name": "repo1"},
+            {"name": "repo2"},
+        ]
+        with patch(
+            "client.GithubOrgClient._public_repos_url",
+            new_callable=PropertyMock
+        ) as mock_url:
+            mock_url.return_value = "mocked_url"
+            client = GithubOrgClient("testorg")
+            repos = client.public_repos()
+            self.assertEqual(repos, ["repo1", "repo2"])
+            mock_url.assert_called_once()
+            mock_get_json.assert_called_once_with("mocked_url")
