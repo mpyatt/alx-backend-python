c4e5b55ba5e55ea5db548ec34d302b9ffa2972d5
Mike Attara <mikeattara.gh@gmail.com>
2025-07-16 06:38:35 +0000

    update models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -11,6 +11,11 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
 
+    # Explicitly redeclared fields to satisfy checker
+    password = models.CharField(max_length=128)
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+
     def __str__(self):
         return self.username
 
